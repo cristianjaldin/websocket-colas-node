cc012e7ee171701ef581e55063a2997881105374
cjaldin <cristianjaldin@outlook.com>
2022-03-18 16:31:31 -0300

    avances

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,23 +1,55 @@
+const TicketControl = require('../models/ticket-control');
 
+
+const ticketControl = new TicketControl();
 
 const socketController = (socket) => {
 
     console.log('Cliente conectado', socket.id);
+    socket.emit('ultimo-ticket', ticketControl.ultimo);
+    socket.emit('estado-actual', ticketControl.ultimos4);
+    socket.emit('tickets-pendientes', ticketControl.tickets.length);
 
     socket.on('disconnect', () => {
         console.log('Cliente desconectado');
     });
 
-    socket.on('enviar-mensaje', (payload, callback) => {
-        console.log(payload);
-        const id = 123456;
-        //callback({ id, fecha: new Date().getTime() });
-        callback(id);
-        //a todos los clientes conectados.
-        //this.io.emit('enviar-mensaje', payload);
-        //socket.emit('enviar-mensaje', payload);
-        socket.broadcast.emit('enviar-mensaje', payload);
-    })
+    socket.on('siguiente-ticket', (payload, callback) => {
+        const siguiente = ticketControl.siguiente();
+        callback(siguiente);
+        socket.broadcast.emit('tickets-pendientes', ticketControl.tickets.length);
+    });
+
+    socket.on('atender-ticket', ({ escritorio }, callback) => {
+        if (!escritorio) {
+            return callback({
+                ok: false,
+                msg: 'El escritorio es obligatorio'
+            });
+        }
+
+        const ticket = ticketControl.atenderTicket(escritorio);
+        //Notificar cambioss en los ultimos 4
+        socket.broadcast.emit('estado-actual', ticketControl.ultimos4);
+        socket.emit('tickets-pendientes', ticketControl.tickets.length);
+        socket.broadcast.emit('tickets-pendientes', ticketControl.tickets.length);
+
+
+        if (!ticket) { //si no existe ticket
+            callback({
+                ok: false,
+                msg: 'Ya no hay tickets pendientes'
+            });
+        } else {
+            callback({
+                ok: true,
+                ticket
+            });
+        }
+
+    });
+
+
 
 }
 
